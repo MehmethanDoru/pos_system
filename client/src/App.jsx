946fe481937aed2a9d6a3e18cc9bc01d4ecbba3a
mehmethan <mdoru22@gmail.com>
2024-08-07 12:23:26 +0300

    completed categories component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
 import "./App.css";
-import Header from "./components/header/header"
+import Header from "./components/header/header";
+import Categories from "./components/cetegories/categories";
 
 function App() {
   return (
     <div className="body">
-      <Header/>
+      <Header />
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories flex-[0.9] overflow-auto max-h-[calc(100vh-_-115px)] pb-72">
+          <Categories />
+        </div>
+        <div className="products flex-[8]">
+          <div className="">products</div>
+        </div>
+        <div className="">
+          <div>cart totals</div>
+        </div>
+      </div>
     </div>
-
   );
 }
 
---
+++ client/src/components/cetegories/categories.jsx
@@ -0,0 +1,29 @@
+const categories = () => {
+  return (
+    <ul className="flex flex-col gap-6">
+        <li className="bg-green-600 py-8 px-10 text-white cursor-pointer hover:bg-rose-500 transition-all text-center">
+            <span>Tümü</span>
+        </li>
+        <li className="bg-green-600 py-8 px-10 text-white cursor-pointer hover:bg-rose-500 transition-all text-center">
+            <span>Kebap</span>
+        </li>
+        <li className="bg-green-600 py-8 px-10 text-white cursor-pointer hover:bg-rose-500 transition-all text-center">
+            <span>Pide</span>
+        </li>
+        <li className="bg-green-600 py-8 px-10 text-white cursor-pointer hover:bg-rose-500 transition-all text-center">
+            <span>Fırın</span>
+        </li>
+        <li className="bg-green-600 py-8 px-10 text-white cursor-pointer hover:bg-rose-500 transition-all text-center">
+            <span>Tatlı</span>
+        </li>
+        <li className="bg-green-600 py-8 px-10 text-white cursor-pointer hover:bg-rose-500 transition-all text-center">
+            <span>İçecek</span>
+        </li>
+        <li className="bg-green-600 py-8 px-10 text-white cursor-pointer hover:bg-rose-500 transition-all text-center">
+            <span>Meze</span>
+        </li>
+    </ul>
+  )
+}
+
+export default categories
