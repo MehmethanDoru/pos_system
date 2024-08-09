e57dd58edcf8124fa660f178b1f0445bec05dd71
mehmethan <mdoru22@gmail.com>
2024-08-09 10:57:51 +0300

    completed customer page

--- client/src/pages/CustomersPage.jsx
+++ client/src/pages/CustomersPage.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
-
+import { Table } from "antd";
 const CustomersPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const dataSource = [
+    {
+      key: "1",
+      name: "Mike",
+      age: 32,
+      address: "10 Downing Street",
+    },
+    {
+      key: "2",
+      name: "John",
+      age: 42,
+      address: "10 Downing Street",
+    },
+  ];
 
-export default CustomersPage
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+  ];
+
+  return (
+    <>
+      <div className="px-6">
+        <h1 className="text-4xl font-bold text-center mb-4">Customers</h1>
+        <Table
+          dataSource={dataSource}
+          columns={columns}
+          bordered
+          pagination={false}
+        />
+      </div>
+    </>
+  );
+};
+
+export default CustomersPage;
